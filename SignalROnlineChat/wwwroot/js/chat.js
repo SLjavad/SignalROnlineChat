73c74f8c3668ae411193e0e785b6f62c330b2f8f
Javad <jd.mn98@yahoo.com>
2020-04-10 16:45:39 +0430

    initial commit

---
+++ SignalROnlineChat/wwwroot/js/chat.js
@@ -0,0 +1,11 @@
+﻿"use strict";
+
+import { signalR } from "./signalr/dist/browser/signalr";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chathub").build();
+
+document.getElementById("sendButton").disabled = true;
+
+connection.on("RecMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+});
